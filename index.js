17cb548b47adfd8e88f2d218c24eda43afac4b80
Kirill Romanenko <romanenko.kirill1995@gmail.com>
2021-10-24 20:51:57 +0300

    receiving the amount of transfers in block 705805

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 const axios = require('axios');
 
-
-axios.get('https://blockchain.info/latestblock')
-    .then(function (response) {
-        // handle success
-        let latestNumberBlock = response.data.block_index;
-    })
-    .catch(function (error) {
-        // handle error
+async function getValueBlock(){
+    try {
+        const response = await axios.get('https://blockchain.info/rawblock/705805');
+        const txLength = response.data.tx.length; // количество транзакций
+        let attentionPrice = [];
+        for (let i = 0; i < txLength; i++) {
+            let countOutAddr = response.data.tx[i].out.length; // количество адресов назначения перевода
+            let price = 0;
+            for (let j = 0; j < countOutAddr; j++) {
+                price += response.data.tx[i].out[j].value;
+                if (price >= 7034232952) {
+                    attentionPrice.push(price); // сумма
+                }
+            }
+            
+        }
+        console.log(attentionPrice);
+        // console.log(response.data.tx[1].out[0].value);
+        // console.log(response.data.tx.length);
+        // console.log(response.data.tx[2].out.length);
+    } catch (error) {
         console.log(error);
-    })
-    .then(function () {
-        // always executed
-    });
+    }
+}
+getValueBlock();
+
+async function getNumberLatestBlock(){
+    try {
+        const response = await axios.get('https://blockchain.info/latestblock');
+        // console.log(response.data.block_index);
+        const latestNumberBlock = response.data.block_index;
+        const startNumberBlock = 705805;
+    } catch (error) {
+        console.log(error);
+    }
+}
+// getNumberLatestBlock();
