99a64b04c1eef120f53602e3c588d7865a0575ff
Kirill Romanenko <romanenko.kirill1995@gmail.com>
2021-10-26 00:19:05 +0300

    create function report

--- index.js
+++ index.js
@@ -1,4 +1,54 @@
 const axios = require('axios');
+const xl = require('excel4node');
+
+function createReport(hash, btcValue, time){
+    const wb = new xl.Workbook();
+    const ws = wb.addWorksheet('Sheet 1');
+    const styleHeader = wb.createStyle({
+        font: {
+            bold: true,
+            color: '#000000',
+            name: 'Times New Roman',
+            size: 14,
+        },
+        numberFormat: '$#,##0.00; ($#,##0.00); -',
+      });
+    const style = wb.createStyle({
+        font: {
+            color: '#000000',
+            name: 'Times New Roman',
+            size: 12,
+        },
+        numberFormat: '$#,##0.00; ($#,##0.00); -',
+      });
+    ws.cell(1, 1) //A1
+      .string('Hash address')
+      .style(styleHeader);
+    ws.cell(1, 2) //B1
+      .string('BTC value')
+      .style(styleHeader);
+    ws.cell(1, 3) //C1
+      .string('Time')
+      .style(styleHeader);
+    
+    hashLength = hash.length;
+    for (let i = 0; i < hashLength; i++) {
+        ws.cell(i+2, 1) //A
+        .string(hash)
+        .style(style);
+        
+        ws.cell(i+2, 2) //B
+        .string(btcValue)
+        .style(style);
+        
+        ws.cell(i+2, 3) //C
+        .string(time)
+        .style(style);
+        
+    }
+    
+    wb.write('Excel.xlsx');
+}
 
 async function getValueBlock(){
     try {
